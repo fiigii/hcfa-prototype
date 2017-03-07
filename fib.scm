2b494c1c978d690cd9f9243ef61e42af1ddee242
Fei Peng <fei.peng@intel.com>
2017-03-06 22:50:10 -0800

    Update Readme

--- fib.scm
+++ fib.scm
@@ -1,8 +1,10 @@
-(define (fact x)
-  (if (< x 1)
+(define (fib n)
+  (if (<= n 1)
       1
-      (* x (fact (- x 1)))))
+      (+ (fib (- n 1) (- n 2)))))
 
-(define (f n) (fact n))
+(define a (fib 2))
+(define b (fib 10))
+(define c (fib 5))
 
-(define a (f 10))
+
